d37ed24d1703cff37a6db4b350f62c810585db22
Jesse Millar <jesse@jessemillar.com>
2013-06-26 21:46:28 -0500

    Fixed the tilemap system.

--- map.js
+++ map.js
@@ -1,17 +1,8 @@
-currentX = 0;
-
 function drawMap(mapString, mapWidth, mapHeight, tileSize) {
-    for (currentTile = 0; currentTile < mapWidth * mapHeight; currentTile++) {        
-        
-        if (currentX < mapWidth - 1) {
-            currentX++;
-        } else {
-            currentX = 0;
-        }
-    
+    for (currentTile = 0; currentTile < mapWidth * mapHeight; currentTile++) {
         if (mapString.charAt(currentTile) == 0) {
             canvas.fillStyle = colorGreen;
-            canvas.fillRect(currentX * tileSize * setup.scale, Math.floor(currentTile / mapWidth) * tileSize * setup.scale, tileSize * setup.scale, tileSize * setup.scale);
+            canvas.fillRect((currentTile - (Math.floor(currentTile / mapWidth) * mapWidth)) * tileSize * setup.scale, Math.floor(currentTile / mapWidth) * tileSize * setup.scale, tileSize * setup.scale, tileSize * setup.scale);
         }
     }
 }
