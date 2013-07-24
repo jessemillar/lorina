36c0b7556724e5a22c805c09b0e248335b50b431
Jesse Millar <jesse@jessemillar.com>
2013-07-23 21:31:01 -0500

    Image preloader works.  Documentation to follow.

--- preloader.js
+++ preloader.js
@@ -1,12 +1,14 @@
 imagesLoaded = 0;
+totalImages = 0;
 
 function preload(imageArray) {
     log("Loading");
-    totalImages = Object.keys(imageArray).length;
+    totalImages += Object.keys(imageArray).length;
     
     for (var key in imageArray) {        
         var spriteReference = new Image();
         spriteReference.src = imageArray[key];
+        imageArray[key] = spriteReference;
         
         spriteReference.onload = function () {
             imagesLoaded++;
