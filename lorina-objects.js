47026bc1d8787e07ee41a50bc00b0de103e4d699
Jesse Millar <jesse@jessemillar.com>
2014-02-14 22:56:55 -0700

    Moving objects works now.

--- lorina-objects.js
+++ lorina-objects.js
@@ -7,89 +7,77 @@ l.make = function(name, kind, x, y, sprite, width, height)
         thingy.kind = kind
         thingy.x = x
         thingy.y = y
-        thingy.z = 0 // Modify mostly with automatic functions
         thingy.width = width
         thingy.height = height
         thingy.sprite = new Image()
             thingy.sprite.src = sprite
-        thingy.rotation = 0 // Change later with other functions
-        thingy.bound = new Object()
-            thingy.bound.x = x
-            thingy.bound.y = y
-            thingy.bound.width = width
-            thingy.bound.height = height
-    
-    if (thingy.bound.width < 3 || thingy.bound.height < 3) {
-        console.log('Objects must be at least 3px by 3px to have proper collision detection')
-    }
-    else
+    l.entities.push(thingy)
+}
+
+l.move = new Object()
+
+l.move.snap = function(name, x, y)
+{
+    for (var i = 0; i < l.entities.length; i++)
     {
-        l.entities.push(thingy)
+        if (l.entities[i].name == name)
+        {
+            l.entities[i].x = x
+            l.entities[i].y = y
+            break
+        }
+    }
+}
+
+l.move.up = function(name, speed)
+{
+    for (var i = 0; i < l.entities.length; i++)
+    {
+        if (l.entities[i].name == name)
+        {
+            l.entities[i].y -= speed
+            break
+        }
+    }
+}
+
+l.move.down = function(name, speed)
+{
+    for (var i = 0; i < l.entities.length; i++)
+    {
+        if (l.entities[i].name == name)
+        {
+            l.entities[i].y += speed
+            break
+        }
+    }
+}
+
+l.move.left = function(name, speed)
+{
+    for (var i = 0; i < l.entities.length; i++)
+    {
+        if (l.entities[i].name == name)
+        {
+            l.entities[i].x -= speed
+            break
+        }
+    }
+}
+
+l.move.right = function(name, speed)
+{
+    for (var i = 0; i < l.entities.length; i++)
+    {
+        if (l.entities[i].name == name)
+        {
+            l.entities[i].x += speed
+            break
+        }
     }
 }
 
 /*
-function bound(objectName, xBound, yBound, wBound, hBound) {
-    objectName.boundX = objectName.x + xBound;
-    objectName.boundY = objectName.y + yBound;
-    objectName.boundW = wBound;
-    objectName.boundH = hBound;
-    
-    if (objectName.boundW < 3 || objectName.boundH < 3) {
-        log("Objects should be at least 3px by 3px to have proper collision detection");
-    }
-}
-
-function move(objectName, moveDirection, moveSpeed) {
-    if (objectName.length > 1) {
-        for (var i = 0; i < objectName.length; i++) {
-            if (moveDirection == 'up') {
-                objectName[i].y = objectName[i].y - moveSpeed;
-                objectName[i].boundY = objectName[i].boundY - moveSpeed;
-            } else if (moveDirection == 'down') {
-                objectName[i].y = objectName[i].y + moveSpeed;
-                objectName[i].boundY = objectName[i].boundY + moveSpeed;
-            } else if (moveDirection == 'left') {
-                objectName[i].x = objectName[i].x - moveSpeed;
-                objectName[i].boundX = objectName[i].boundX - moveSpeed;
-            } else if (moveDirection == 'right') {
-                objectName[i].x = objectName[i].x + moveSpeed;
-                objectName[i].boundX = objectName[i].boundX + moveSpeed;
-            }
-        }
-    } else {
-        if (moveDirection == 'up') {
-            objectName.y = objectName.y - moveSpeed;
-            objectName.boundY = objectName.boundY - moveSpeed;
-        } else if (moveDirection == 'down') {
-            objectName.y = objectName.y + moveSpeed;
-            objectName.boundY = objectName.boundY + moveSpeed;
-        } else if (moveDirection == 'left') {
-            objectName.x = objectName.x - moveSpeed;
-            objectName.boundX = objectName.boundX - moveSpeed;
-        } else if (moveDirection == 'right') {
-            objectName.x = objectName.x + moveSpeed;
-            objectName.boundX = objectName.boundX + moveSpeed;
-        }
-    }
-}
-
-function snap(objectName, destinationX, destinationY) {
-    if (objectName.length > 1) {
-        for (var i = 0; i < objectName.length; i++) {
-            objectName[i].x = destinationX;
-            objectName[i].boundX = destinationX;
-            objectName[i].y = destinationY;
-            objectName[i].boundY = destinationY;
-        }
-    } else {
-        objectName.x = destinationX;
-        objectName.boundX = destinationX;
-        objectName.y = destinationY;
-        objectName.boundY = destinationY;
-    }
-}
-
 function measure(objectA, objectB, desiredMeasure) {
     var distanceX = Math.floor(Math.abs((objectA.x + objectA.w / 2) - (objectB.x + objectB.w / 2)));
     var distanceY = Math.floor(Math.abs((objectA.y + objectA.h / 2) - (objectB.y + objectB.h / 2)));
