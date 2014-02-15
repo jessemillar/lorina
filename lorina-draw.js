5ea2ee3bbce782ff33a7f4e949709880d1c86c5b
Jesse Millar <jesse@jessemillar.com>
2014-02-15 11:32:33 -0700

    Optimized drawing and enabled touchscreen interaction.

--- lorina-draw.js
+++ lorina-draw.js
@@ -6,28 +6,34 @@ l.draw.blank = function()
     l.ctx.fillRect(l.camera.x, l.camera.y, l.camera.width, l.camera.height)
 }
 
-l.draw.rectangle = function(x, y, width, height, color, opacity)
+l.draw.rectangle = function(x, y, width, height, color, opacity) // Mainly for debug purposes
 {
-    if (opacity)
+    if (x + width > l.camera.x && x < l.camera.width && y + height > l.camera.y && y < l.camera.height) // Only draw if visible on the screen
     {
-        l.ctx.globalAlpha = opacity
-    }
-    l.ctx.fillStyle = color
-    l.ctx.fillRect(Math.round(x), Math.round(y), width, height)
-    if (opacity)
-    {
-        l.ctx.globalAlpha = 1 // Reset the opacity after drawing the rectangle
+        if (opacity)
+        {
+            l.ctx.globalAlpha = opacity
+        }
+        l.ctx.fillStyle = color
+        l.ctx.fillRect(Math.round(x), Math.round(y), width, height)
+        if (opacity)
+        {
+            l.ctx.globalAlpha = 1 // Reset the opacity after drawing the rectangle
+        }
     }
 }
 
 l.draw.object = function(name)
 {
-    l.ctx.drawImage(l.entities[name].sprite, Math.round(l.entities[name].x), Math.round(l.entities[name].y))
-    if (l.game.debug)
+    if (Math.round(l.entities[name].x + l.entities[name].width) > l.camera.x && Math.round(l.entities[name].x) < l.camera.width && Math.round(l.entities[name].y + l.entities[name].height) > l.camera.y && Math.round(l.entities[name].y) < l.camera.height)  // Only draw if visible on the screen
     {
-        l.draw.rectangle(l.entities[name].x + l.entities[name].bounding.offset.x, l.entities[name].y + l.entities[name].bounding.offset.y, l.entities[name].bounding.width, l.entities[name].bounding.height, '#00ff00', 0.5)
-        l.draw.rectangle(l.entities[name].x, l.entities[name].y, 1, 1, '#0000ff')
-        l.draw.rectangle(l.entities[name].anchor.x, l.entities[name].anchor.y, 1, 1, '#ffff00')
-        l.text.write(name, l.entities[name].x + l.entities[name].width, l.entities[name].y, '#ff0000')
+        l.ctx.drawImage(l.entities[name].sprite, Math.round(l.entities[name].x), Math.round(l.entities[name].y))
+        if (l.game.debug)
+        {
+            l.draw.rectangle(l.entities[name].x + l.entities[name].bounding.offset.x, l.entities[name].y + l.entities[name].bounding.offset.y, l.entities[name].bounding.width, l.entities[name].bounding.height, '#00ff00', 0.5)
+            l.draw.rectangle(l.entities[name].x, l.entities[name].y, 1, 1, '#0000ff')
+            l.draw.rectangle(l.entities[name].anchor.x, l.entities[name].anchor.y, 1, 1, '#ffff00')
+            l.text.write(name, l.entities[name].x + l.entities[name].width, l.entities[name].y, '#ff0000')
+        }
     }
 }
---
+++ lorina-touches.js
@@ -0,0 +1,37 @@
+l.touches = new Object() // Group the touch-related functions
+
+l.touches.database = new Array() // Keep track of where we're touching on the screen
+
+l.touches.enable = function()
+{
+    document.addEventListener('touchstart', l.touches.touched(event), false)
+    document.addEventListener('touchmove', l.touches.touched(event), false)
+    document.addEventListener('touchend', l.touches.cancel(), false)
+    document.addEventListener('touchcancel', l.touches.cancel(), false)
+}
+
+l.touches.touched = function(event)
+{
+    if (event)
+    {
+        l.touches.database = event.touches
+        l.touches.debug()
+    }
+}
+
+l.touches.cancel = function(event)
+{
+    l.touches.database.length = 0
+    l.touches.debug()
+}
+
+l.touches.debug = function()
+{
+    if (l.game.debug && l.touches.database.length > 0)
+    {
+        for (var i = 0; i < l.touches.database.length; i++)
+        {
+            console.log('X' + i = ': ' + l.touches.database[i].clientX + ' Y' + i + ': ' + l.touches.database[i].clientY)
+        }
+    }
+}
