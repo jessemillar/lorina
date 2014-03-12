056da5e88c37e7e374f2bdcc8fcb4131e2e54e6f
Jesse Millar <jesse@jessemillar.com>
2014-03-12 10:29:23 -0600

    The position and size of the canvas is now more manageable and pretty.

--- Theo/backend.js
+++ Theo/backend.js
@@ -1,27 +1,97 @@
 t = new Object() // Group everything here
 
-t.setup = function(color, grid, opacity, width, height)
+t.grid = new Object()
+	t.grid.x = new Array() // Put x coordinates here
+	t.grid.y = new Array() // Put y coordinates here
+
+t.setup = function(tileWidth, tileHeight, width, height, backgroundColor, gridColor, gridOpacity)
 {
-	t.color = color
-	t.grid = new Object()
-		t.grid.width = width
-		t.grid.height = height
-		t.grid.color = grid
-		t.grid.opacity = opacity / 100
-
-	document.body.style.background = t.color
-
 	t.dom = document.getElementById('canvas')
     t.ctx = document.getElementById('canvas').getContext('2d')
 
-    t.dom.width = window.innerWidth
-    t.dom.height = window.innerHeight - 50
+    document.body.setAttribute('onresize', 't.calculate.canvas()')
+
+	t.color = backgroundColor
+	t.grid = new Object()
+		t.grid.width = width
+		t.grid.height = height
+		t.grid.tile = new Object()
+			t.grid.tile.width = tileWidth
+			t.grid.tile.height = tileHeight
+		t.grid.color = gridColor
+		t.grid.opacity = gridOpacity / 100
+
+	t.calculate.canvas()
 }
 
 t.start = function()
 {
 	t.loop = setInterval(main, 1000 / 60)
 }
+
+t.calculate = new Object() // Group the calculation functions
+
+t.calculate.canvas = function()
+{
+	if (!t.canvas)
+	{
+		t.canvas = new Object()
+	}
+		t.canvas.width = t.grid.width * t.grid.tile.width
+		t.canvas.height = t.grid.height * t.grid.tile.height
+
+	if (t.canvas.width >= window.innerWidth)
+	{
+		document.getElementById('toolbar').style.width = t.canvas.width
+	}
+
+	if (t.canvas.width >= window.innerWidth && t.canvas.height >= window.innerHeight - 50)
+	{
+		console.log('Fullscreen')
+		document.body.style.background = t.color
+		t.dom.style.left = 0
+	    t.dom.style.top = 50
+		t.dom.width = t.canvas.width
+	    t.dom.height = t.canvas.height
+	}
+	else
+	{
+		if (t.canvas.width >= window.innerWidth)
+		{
+			document.body.style.background = '#111111'
+			t.dom.width = t.canvas.width
+	    	t.dom.height = t.canvas.height
+	    	t.dom.style.left = 0
+	    	t.dom.style.top = (window.innerHeight - 50) / 2 - t.dom.height / 2 + 50
+		}
+		else if (t.canvas.height >= window.innerHeight - 50)
+		{
+			document.body.style.background = '#111111'
+			t.dom.width = t.canvas.width
+	    	t.dom.height = t.canvas.height
+	    	t.dom.style.left = window.innerWidth / 2 - t.dom.width / 2
+	    	t.dom.style.top = 50
+		}
+		else
+		{
+			document.body.style.background = '#111111'
+			t.dom.width = t.canvas.width
+	    	t.dom.height = t.canvas.height
+	    	t.dom.style.left = window.innerWidth / 2 - t.dom.width / 2
+	    	t.dom.style.top = (window.innerHeight - 50) / 2 - t.dom.height / 2 + 50
+		}
+	}
+}
+
+/*
+t.calculate.grid = function()
+{
+	var currentX = 
+	var currentY = 
+
+	t.grid['id' + currentY * gridWidth + currentX] = new Object()
+}
+*/
 
 t.draw = new Object() // Group the draw functions
 
@@ -31,27 +101,49 @@ t.draw.clear = function()
 	t.ctx.fillRect(0, 0, t.dom.width, t.dom.height)
 }
 
+t.draw.line = function(x1, y1, x2, y2, width, color, opacity)
+{
+	t.ctx.beginPath()
+	t.ctx.moveTo(Math.round(x1) + 0.5, Math.round(y1) + 0.5)
+	t.ctx.lineTo(Math.round(x2) + 0.5, Math.round(y2) + 0.5)
+	t.ctx.lineWidth = width
+	t.ctx.strokeStyle = '#' + color
+	if (opacity)
+	{
+		t.ctx.globalAlpha = opacity
+	}
+	else
+	{
+		t.ctx.globalAlpha = 1
+	}
+	t.ctx.stroke()
+}
+
+t.draw.rectangle = function(x, y, width, height, color, opacity)
+{
+	t.ctx.beginPath()
+	t.ctx.rect(x, y, width, height)
+	t.ctx.fillStyle = color
+	if (opacity)
+	{
+		t.ctx.globalAlpha = opacity
+	}
+	else
+	{
+		t.ctx.globalAlpha = 1
+	}
+	t.ctx.fill()
+}
+
 t.draw.grid = function()
 {
-	for (var i = 0; i < t.dom.width / t.grid.width; i++)
+	for (var i = 1; i < t.grid.width; i++)
 	{
-		t.ctx.beginPath()
-		t.ctx.moveTo(Math.round(i * t.grid.width) + 0.5, 0 + 0.5)
-		t.ctx.lineTo(Math.round(i * t.grid.width) + 0.5, Math.round(t.dom.height) + 0.5)
-		t.ctx.lineWidth = 1
-		t.ctx.strokeStyle = '#' + t.grid.color
-		t.ctx.globalAlpha = t.grid.opacity
-		t.ctx.stroke()
+		t.draw.line(i * t.grid.tile.width, 0, i * t.grid.tile.width, t.canvas.height, 1, t.grid.color, t.grid.opacity)
 	}
 
-	for (var i = 0; i < t.dom.height / t.grid.height; i++)
+	for (var i = 1; i < t.grid.height; i++)
 	{
-		t.ctx.beginPath()
-		t.ctx.moveTo(0 + 0.5, Math.round(i * t.grid.height) + 0.5)
-		t.ctx.lineTo(Math.round(t.dom.width) + 0.5, Math.round(i * t.grid.height) + 0.5)
-		t.ctx.lineWidth = 1
-		t.ctx.strokeStyle = '#' + t.grid.color
-		t.ctx.globalAlpha = t.grid.opacity
-		t.ctx.stroke()
+		t.draw.line(0, i * t.grid.tile.height, t.canvas.width, i * t.grid.tile.height, t.grid.color, t.grid.opacity)
 	}
 }
--- Theo/main.js
+++ Theo/main.js
@@ -1,11 +1,7 @@
-t.setup('2ECC40', '3D9970', 50, 35, 35)
+t.setup(35, 35, 15, 15, '7FDBFF', '39CCCC', 50)
 t.start()
 
-document.getElementById('tile-width').value = t.grid.width
-document.getElementById('tile-height').value = t.grid.height
-document.getElementById('background-color').value = t.color
-document.getElementById('grid-color').value = t.grid.color
-document.getElementById('grid-opacity').value = t.grid.opacity * 100
+updateUI()
 
 function main()
 {
@@ -24,11 +20,73 @@ function closeHat()
 	document.getElementById('hat').style.display = 'none'
 }
 
+function updateUI()
+{
+	document.getElementById('tile-width').value = t.grid.tile.width
+	document.getElementById('tile-height').value = t.grid.tile.height
+	document.getElementById('grid-width').value = t.grid.width
+	document.getElementById('grid-height').value = t.grid.height
+	document.getElementById('background-color').value = t.color
+	document.getElementById('grid-color').value = t.grid.color
+	document.getElementById('grid-opacity').value = t.grid.opacity * 100
+}
+
 function updatePreferences()
 {
-	t.grid.width = document.getElementById('tile-width').value
-	t.grid.height = document.getElementById('tile-height').value
+	t.grid.tile.width = document.getElementById('tile-width').value
+	t.grid.tile.height = document.getElementById('tile-height').value
+	t.grid.width = document.getElementById('grid-width').value
+	t.grid.height = document.getElementById('grid-height').value
 	t.color = document.getElementById('background-color').value
 	t.grid.color = document.getElementById('grid-color').value
 	t.grid.opacity = document.getElementById('grid-opacity').value / 100
+
+	if (t.grid.tile.width > 999)
+	{
+		t.grid.tile.width = 999
+	}
+	else if (t.grid.tile.width < 1)
+	{
+		t.grid.tile.width = 1
+	}
+
+	if (t.grid.tile.height > 999)
+	{
+		t.grid.tile.height = 999
+	}
+	else if (t.grid.tile.height < 1)
+	{
+		t.grid.tile.height = 1
+	}
+
+	if (t.grid.width > 999)
+	{
+		t.grid.width = 999
+	}
+	else if (t.grid.width < 1)
+	{
+		t.grid.width = 1
+	}
+
+	if (t.grid.height > 999)
+	{
+		t.grid.height = 999
+	}
+	else if (t.grid.height < 1)
+	{
+		t.grid.height = 1
+	}
+
+	if (t.grid.opacity > 100)
+	{
+		t.grid.opacity = 100
+	}
+	else if (t.grid.opacity < 0)
+	{
+		t.grid.opacity = 0
+	}
+
+	t.calculate.canvas()
+
+	updateUI()
 }
