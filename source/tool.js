f34e73e83307199af1ccfb58259d8a4608c237df
Jesse Millar <jesse@jessemillar.com>
2014-04-15 14:14:21 -0600

    Added measurement functions.

--- source/tool.js
+++ source/tool.js
@@ -4,4 +4,22 @@ var Tool = function()
     {
         return Math.random() * (max - min) + min
     }
+
+    this.measureX = function(a, b)
+    {
+    	return Math.abs(a.x - b.x)
+    }
+
+    this.measureY = function(a, b)
+    {
+    	return Math.abs(a.y - b.y)
+    }
+
+    this.measure = function(a, b)
+    {
+    	var x = this.measureX(a, b)
+    	var y = this.measureY(a, b)
+
+    	return Math.sqrt(x * x + y * y)
+    }
 }
