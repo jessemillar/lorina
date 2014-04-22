3ac5ab1c117a4eef184cc7ef35ea8539c7de5625
Jesse Millar <jesse@jessemillar.com>
2014-04-22 13:14:31 -0500

    Fixed the Hello World demo.

--- demos/Hello World/game.js
+++ demos/Hello World/game.js
@@ -1,9 +1,17 @@
-l.game.setup('#FF0000', false, false, true) // Start the game engine and set the default blank color
+var game = new Lorina()
+	game.setColor('#FF0000')
+		.makeFullscreen()
 
-l.game.start('game') // Tell the preloader to load everything and then go to the 'game' screen
+var typewriter = new Typewriter()
+	typewriter.setFont('Helvetica')
+			  .setAlignment('center')
+			  .setSize(30)
+			  .setColor('#FFFFFF')
 
-l.screen.game = function() // This is our 'game' screen that's run by the preloader
+var main = function() // This is our 'game' screen that's run by the preloader
 {
-	l.draw.blank() // Blank the screen with the color set in l.game.setup before drawing
-	l.write.hud('Hello, World', 50, 50, 'Helvetica', 30, '#FFFFFF') // Write 'Hello, World' to the screen
+	game.blank() // Blank the screen with the color set in l.game.setup before drawing
+	typewriter.setPosition(l.room.width / 2, l.room.height / 2).write('Hello, World') // Write 'Hello, World' to the screen
 }
+
+game.start(main)
