bc83287c9284ef2959d5d5cf99dae497f6067b41
Jesse Millar <jesse@jessemillar.com>
2014-04-25 00:18:55 -0500

    Gamepad fixes.

--- source/gamepad.js
+++ source/gamepad.js
@@ -7,10 +7,16 @@ var Gamepad = function()
         l.gamepads = navigator.webkitGetGamepads()
 
         var i = l.gamepads[0]
-        this.p1 = {buttons: {playstation: i.buttons[16], x: i.buttons[0], circle: i.buttons[1], square: i.buttons[2], triangle: i.buttons[3], l1: i.buttons[4], l2: i.buttons[6], r1: i.buttons[5], r2: i.buttons[7], select: i.buttons[8], start: i.buttons[9], up: i.buttons[12], down: i.buttons[13]}, left: i.buttons[14], right: i.buttons[15], joysticks: {left: {horizontal: i.axes[0], vertical: i.axes[1], button: i.buttons[10]}, right: {horizontal: i.axes[2], vertical: i.axes[3], button: i.buttons[11]}}}
+        if (i)
+        {
+            this.p1 = {buttons: {playstation: i.buttons[16], x: i.buttons[0], circle: i.buttons[1], square: i.buttons[2], triangle: i.buttons[3], l1: i.buttons[4], l2: i.buttons[6], r1: i.buttons[5], r2: i.buttons[7], select: i.buttons[8], start: i.buttons[9], up: i.buttons[12], down: i.buttons[13]}, left: i.buttons[14], right: i.buttons[15], joysticks: {left: {horizontal: i.axes[0], vertical: i.axes[1], button: i.buttons[10]}, right: {horizontal: i.axes[2], vertical: i.axes[3], button: i.buttons[11]}}}
+        }
 
-        var i = l.gamepads[3]
-        this.p2 = {buttons: {playstation: i.buttons[16], x: i.buttons[0], circle: i.buttons[1], square: i.buttons[2], triangle: i.buttons[3], l1: i.buttons[4], l2: i.buttons[6], r1: i.buttons[5], r2: i.buttons[7], select: i.buttons[8], start: i.buttons[9], up: i.buttons[12], down: i.buttons[13]}, left: i.buttons[14], right: i.buttons[15], joysticks: {left: {horizontal: i.axes[0], vertical: i.axes[1], button: i.buttons[10]}, right: {horizontal: i.axes[2], vertical: i.axes[3], button: i.buttons[11]}}}
+        var i = l.gamepads[1]
+        if (i)
+        {
+            this.p2 = {buttons: {playstation: i.buttons[16], x: i.buttons[0], circle: i.buttons[1], square: i.buttons[2], triangle: i.buttons[3], l1: i.buttons[4], l2: i.buttons[6], r1: i.buttons[5], r2: i.buttons[7], select: i.buttons[8], start: i.buttons[9], up: i.buttons[12], down: i.buttons[13]}, left: i.buttons[14], right: i.buttons[15], joysticks: {left: {horizontal: i.axes[0], vertical: i.axes[1], button: i.buttons[10]}, right: {horizontal: i.axes[2], vertical: i.axes[3], button: i.buttons[11]}}}
+        }
 
         this.applyDeadzone()
 
