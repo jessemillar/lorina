67358944e4f7cc56fbadb45d3101ef7db02e2aaf
Jesse Millar <hellojessemillar@gmail.com>
2014-08-12 08:45:51 -0600

    Push cleanup

--- source/pusher.js
+++ source/pusher.js
@@ -1,8 +1,12 @@
 var Pusher = function() // Support for CocoonJS local push notifications
 {
+	this.registered = false
+
 	this.register = function()
 	{
 		CocoonJS.Notification.registerForPushNotifications()
+
+		this.registered = true
 
 		return this
 	}
@@ -11,27 +15,37 @@ var Pusher = function() // Support for CocoonJS local push notifications
 	{
 		CocoonJS.Notification.unregisterForPushNotifications()
 
+		this.registered = false
+
 		return this
 	}
 
 	this.notify = function(message, delay) // Delay is in seconds
 	{
-		var sound = true
-		var badge = 0
-		var userData = {item: 0}
+		if (this.registered)
+		{
+			var sound = true
+			var badge = 0
+			var userData = {item: 0}
 
-		var scheduleTime = new Date().getTime() + delay * 1000
+			var scheduleTime = new Date().getTime() + delay * 1000
 
-		var localNotification = CocoonJS.Notification.createLocalNotification( 
-			message,
-			sound,
-			badge,
-			userData,
-			'This is the local notification body',
-			'Local notification title',
-			scheduleTime
-		)
+			var localNotification = CocoonJS.Notification.createLocalNotification( 
+				message,
+				sound,
+				badge,
+				userData,
+				'This is the local notification body',
+				'Local notification title',
+				scheduleTime
+			)
 
-        CocoonJS.Notification.sendLocalNotification(localNotification)
+	        CocoonJS.Notification.sendLocalNotification(localNotification)
+		}
+	}
+
+	this.cancel = function() // Cancels all local notifications because I'm too lazy to figure out how to cancel just one
+	{
+		CocoonJS.Notification.cancelAllLocalNotifications()
 	}
 }
